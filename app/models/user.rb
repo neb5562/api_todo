6d0aa7c9e3a7a47842607a4157ba1097ad16fe45
neb5562 <gnebieridzs@unisens.ge>
2022-07-15 11:06:20 +0400

    setup bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :username, uniqueness: true, presence: true, length: { minimum: 3, maximum: 50}, format: { without: /\s/ }
   validates :password_digest, presence: true, length: { minimum: 8 }
 end
